2f1d4af1b85ed4069833170ea726edaea34bd10a
michihiro imaoka <imaoca@gmail.com>
2023-11-15 09:12:46 +0900

    Update instructions.v
    // EI(Enable Interrupt) DI(Disable Interrupt)
    
    // TBD Key repeat
    // TBD Hold mode
    // TBD STR  ram[8'adr],regs(sss)
    // TBD LD   regs(sss),ram[8'adr]
    // TBD ret

--- instructions_dl167/src/instructions.v
+++ instructions_dl167/src/instructions.v
@@ -1,11 +1,9 @@
-// Increase clock frequency in game mode.
-// Interrupt 
+// EI(Enable Interrupt) DI(Disable Interrupt)
 
+// TBD Key repeat
+// TBD Hold mode
 // TBD STR  ram[8'adr],regs(sss)
 // TBD LD   regs(sss),ram[8'adr]
-// TBD Key repeat
-// TBD EI(Enable Interrupt) DI(Disable Interrupt)
-// TBD Hold mode
 // TBD ret 
 
 module cpu(input rst,mode,clk,Abtn,Bbtn,[3:0]btn,output low,[7:0]col, output [7:0]row,output [5:0]leds);
@@ -31,10 +29,10 @@ module cpu(input rst,mode,clk,Abtn,Bbtn,[3:0]btn,output low,[7:0]col, output [7:
 
     ram[0] <=8'b1101_0000;  // jmp 16 go to code
     ram[1] <=16;            // 16
-    ram[2] <=8'b1101_0000;  // jmp 24 right key   
-    ram[3] <=24;
-    ram[4] <=8'b1101_0000;  // jmp 28 left key
-    ram[5] <=28;
+    ram[2] <=8'b1101_0000;  // jmp 25 right key   
+    ram[3] <=25;
+    ram[4] <=8'b1101_0000;  // jmp 30 left key
+    ram[5] <=30;
 
     ram[16] <=8'b1110_0011;  // mvi R3,1110_0000;
     ram[17] <=8'b1110_0000;  // 1110_0000
@@ -44,37 +42,28 @@ module cpu(input rst,mode,clk,Abtn,Bbtn,[3:0]btn,output low,[7:0]col, output [7:
 
     ram[20] <=8'b10_110_010; // vpoke VR6,R2  
     ram[21] <=8'b10_111_011; // vpoke VR7,R3  
-
-    ram[22] <=8'b1101_0000;  // jmp 20 as halt
-    ram[23] <=22;    
-           
-    ram[24] <=8'b01111_011;  // R_rotate R3
-    ram[25] <=8'b01111_010;  // R_rotate R2
+    
+    ram[22] <=8'b1100_0001;  // di=0;
+    ram[23] <=8'b1101_0000;  // jmp 23
+    ram[24] <=23;    
+    
+    ram[25] <=8'b1101_0001;  // di=1;        
+    ram[26] <=8'b01111_011;  // R_rotate R3
+    ram[27] <=8'b01111_010;  // R_rotate R2
             
-    ram[26] <=8'b1101_0000;  // jmp 20
-    ram[27] <=20;
+    ram[28] <=8'b1101_0000;  // jmp 20
+    ram[29] <=20;
  
-    ram[28] <=8'b01110_011;  // L_rotate R3
-    ram[29] <=8'b01110_010;  // L_rotate R2
+    ram[30] <=8'b1101_0001;  // di=1;    
+    ram[31] <=8'b01110_011;  // L_rotate R3
+    ram[32] <=8'b01110_010;  // L_rotate R2
      
-    ram[30] <=8'b1101_0000;  // jmp 20
-    ram[31] <=20;
-/*
+    ram[33] <=8'b1101_0000;  // jmp 20
+    ram[34] <=20;
+
 // DI     8'b1100_0001: di=1;
 // EI     8'b1101_0001: di=0;
-    ram[0] <=8'b1110_0110;  // mvi R6,1110_0000;
-    ram[1] <=8'b1110_0000;  // 1110_0000
-    ram[2] <=8'b1110_0100;  // mvi R4,0100_0000;
-    ram[3] <=8'b0100_0000;  // 0100_0000
 
-    ram[4] <=8'b01111_110;  // rotate R6
-    ram[5] <=8'b01111_100;  // rotate R4
-
-    ram[6] <=8'b10_110_100; // vpoke VR6,R4  
-    ram[7] <=8'b10_111_110; // vpoke VR7,R6  
-    ram[8] <=8'b1101_0000;  // jmp 4
-    ram[9] <=8'b0000_0100;  // 4
-*/
     end
     
 	always @(posedge clock  or negedge rst)
@@ -92,8 +81,8 @@ module cpu(input rst,mode,clk,Abtn,Bbtn,[3:0]btn,output low,[7:0]col, output [7:
 /* NOT */	5'b01101: regs[sss]=!regs[sss];
 /*RROTATE*/	5'b01110: regs[sss]=regs[sss]>>1| (regs[sss]<<7 & 8'b10000000);
 /*LROTATE*/	5'b01111: regs[sss]=regs[sss]<<1| (regs[sss]>>7 & 8'b00000001);	
-/* JNC */	5'b1100z: op2=1;
-/* JMP */   5'b1101z: op2=2;
+//* JNC */	5'b1100z: op2=1;
+//* JMP */  5'b1101z: op2=2;
 /* MVI */	5'b1110z: begin regs[sss]=ram[regs[7]+1];regs[7]=regs[7]+1;end
 /*VPOKE*/   5'b10zzz: vregs[op[2:0]]=regs[sss];      	
 
@@ -101,24 +90,26 @@ module cpu(input rst,mode,clk,Abtn,Bbtn,[3:0]btn,output low,[7:0]col, output [7:
 //*VPOKE*/   5'b10zzzzzz: vregs[op[2:0]]=regs[sss];     
 //* DI */    8'b1100_0001: di=1;
 //* EI */    8'b1101_0001: di=0; 	
-	   endcase		
+	   endcase
+/* JNC */	if (dout==8'b1100_0000)op2=1;
+/* JMP */   if (dout==8'b1101_0000)op2=2;		
+            if (dout==8'b1100_0001) di=0;
+            if (dout==8'b1101_0001) di=1;
        regs[7]=regs[7]+1;
 //        if (dout[7]==0) regs[7]=regs[7]+1;
 	   end
-
        else begin
         if (op2==1) begin regs[7]=(c_flag)?regs[7]+1:dout;c_flag=0;end
         if (op2==2) regs[7]=dout;
         op2=0;
        end
 
-//    if ((op2==0)&&(regs[5]!=0)&&(di==0)) begin 
-    if ((op2==0)&&(regs[5]!=0)) begin 
+    if ((op2==0)&&(regs[5]!=0)&&(di==0)) begin 
+//    if ((op2==0)&&(regs[5]!=0)) begin 
 //        regs[4]=regs[7];
         if (regs[5]&8'b1000_0000) regs[7]=2;
         if (regs[5]&8'b0001_0000) regs[7]=4;
     end
-
 end
     always @(posedge clk) counter <= counter + 1;
 endmodule
